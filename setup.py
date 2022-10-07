8c15b0be18680ec58e5be1d87b9d9745981798dd
AbdealiJK <abdealikothari@gmail.com>
2022-10-07 11:00:10 +0530

    chore: Remove extra deps for flask plugin

--- setup.py
+++ setup.py
@@ -31,9 +31,6 @@ extras_require = {
         'PyMySQL>=0.8.0',
         'six>=1.4.0'
     ],
-    'flask': ['Flask>=0.9'],
-    'flask-login': ['Flask-Login>=0.2.9'],
-    'flask-sqlalchemy': ['Flask-SQLAlchemy>=1.0, <3.0.0'],
     'flexmock': ['flexmock>=0.9.7'],
     'i18n': ['SQLAlchemy-i18n>=0.8.4,!=1.1.0'],
 }
