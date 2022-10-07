e4e535ea19c338fe3fde6bbe38e02fe645749cb9
AbdealiJK <abdealikothari@gmail.com>
2022-10-07 09:55:33 +0530

    refactor: Remove flask plugin
    The plugin is useful for a specific case when using flask and we will
    avoid maintaining it as part of sqla-history.
    This can be a separate package if required later on

--- sqlalchemy_continuum/plugins/__init__.py
+++ sqlalchemy_continuum/plugins/__init__.py
@@ -1,6 +1,5 @@
 from .activity import ActivityPlugin
 from .base import Plugin, PluginCollection
-from .flask import FlaskPlugin
 from .null_delete import NullDeletePlugin
 from .property_mod_tracker import PropertyModTrackerPlugin
 from .transaction_changes import TransactionChangesPlugin
