0c4cf49c6a1c4d0bdc061cbcabb2a9edb8dbb1c1
Indivar Mishra <indimishra@gmail.com>
2022-12-08 18:06:35 +0530

    refactor(#56): Add hybrid property to versioned ORM class
    add hybrid property to versioned object.
    
    test(#56): add testcase to check hybrid_property in version model
    
    Add testcase to check if hybrid property is being added to
    versioned model or not.
    check if mapping method is returning correct attributes or not.

---
+++ tests/test_hybrid_property.py
@@ -0,0 +1,28 @@
+import datetime
+import pytest
+import sqlalchemy as sa
+from sqlalchemy_history.utils import version_class
+
+from tests import TestCase
+
+
+class TestHybridProperty(TestCase):
+    def create_models(self):
+        class Article(self.Model):
+            __tablename__ = "article"
+            __versioned__ = {}
+
+            id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+            name = sa.Column(sa.Unicode(255), nullable=False)
+            content = sa.Column(sa.UnicodeText)
+            description = sa.Column(sa.UnicodeText)
+            publish = sa.Column(sa.DateTime, default=datetime.datetime.utcnow)
+
+            @sa.ext.hybrid.hybrid_property
+            def time_from_publish(self):
+                return datetime.datetime.today() - self.publish
+
+        self.Article = Article
+
+    def test_hybrid_property_mapping_for_versioned_class(self):
+        version_class(self.Article).time_from_publish
