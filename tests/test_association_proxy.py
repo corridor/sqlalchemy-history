0f85de5c5ece6151bfc3e5152756a898224e859c
Indivar Mishra <indimishra@gmail.com>
2022-12-08 16:24:52 +0530

    refactor(#54): Add assoc_proxy attr as prop to versioned model
    Add Association Proxy attributes as property to versioned model
    to be available for versioned model.
    
    Currently relationship attributes are set as property in versioned model due
    to which creating association proxy for versioned model gives an error saying
    relationship not found.
    
    test(#54): Add testcase for association proxy check
    
    Adding testcase for association proxy being added into versioned
    model and detection utility method.

---
+++ tests/test_association_proxy.py
@@ -0,0 +1,44 @@
+import sqlalchemy as sa
+from sqlalchemy_history.utils import get_association_proxies, version_class
+
+from tests import TestCase
+
+
+class TestAssociationProxy(TestCase):
+    def create_models(self):
+        class Article(self.Model):
+            __tablename__ = "article"
+            __versioned__ = {}
+
+            id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+            name = sa.Column(sa.Unicode(255), nullable=False)
+            content = sa.Column(sa.UnicodeText)
+            description = sa.Column(sa.UnicodeText)
+
+            upanaam = sa.ext.associationproxy.association_proxy("tags", "name")
+
+        class Tag(self.Model):
+            __tablename__ = "tag"
+            __versioned__ = {}
+
+            id = sa.Column(sa.Integer, autoincrement=True, primary_key=True)
+            name = sa.Column(sa.Unicode(255))
+            article_id = sa.Column(sa.Integer, sa.ForeignKey(Article.id))
+            article = sa.orm.relationship(Article, backref="tags")
+
+        self.Article = Article
+        self.Tag = Tag
+
+    def test_association_proxy_mapping(self):
+        assoc_mapping = get_association_proxies(self.Article)
+        assert len(assoc_mapping) == 1
+        assert list(assoc_mapping.keys())[0] == "upanaam"
+        assert isinstance(assoc_mapping["upanaam"], sa.ext.associationproxy.AssociationProxy)
+
+    def test_association_proxy_detection(self):
+        """
+        Currently Versioned Model have a proxy mapped as a property due limitation in the way relationships are handled,
+        For now a property in versioned model should be available for proxy attributes of orginal model
+        """
+        assert issubclass(type(self.Article.upanaam), sa.ext.associationproxy.AssociationProxyInstance)
+        assert isinstance(version_class(self.Article).upanaam, property)
