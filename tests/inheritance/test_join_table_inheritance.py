1381eaecb00581991568a1f38ef086d3f8e7e3ce
AbdealiLoKo <abdealikothari@gmail.com>
2023-08-05 18:17:47 +0530

    chore: Fix flake8 issues for type comparisons
    This is a recent check flake8 has added - so, fix it for CI to work

--- tests/inheritance/test_join_table_inheritance.py
+++ tests/inheritance/test_join_table_inheritance.py
@@ -57,9 +57,9 @@ class JoinTableInheritanceTestCase(TestCase):
         self.session.add(textitem)
         self.session.commit()
 
-        # assert type(textitem.versions[0]) == self.TextItemVersion
-        assert type(article.versions[0]) == self.ArticleVersion
-        assert type(blogpost.versions[0]) == self.BlogPostVersion
+        # assert type(textitem.versions[0]) is self.TextItemVersion
+        assert type(article.versions[0]) is self.ArticleVersion
+        assert type(blogpost.versions[0]) is self.BlogPostVersion
 
     def test_all_tables_contain_transaction_id_column(self):
         tx_column = self.options["transaction_column_name"]
