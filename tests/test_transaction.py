6ff270bd38ae36d985838dbd104f4dbc41064db9
Indivar Mishra <indimishra@gmail.com>
2023-08-12 20:11:29 +0530

    chore: stop skipping certains tests
    there were few tests that were being skipped due to following
    reasons:
    1. schema not being created for oracle so added seperate query
       in exception block to create these schema
    2. update column_reflection test to validate column availability
       in versioned class
    3. removed deprecated tests which are only supported sqla<1.4
    4. added reasons for some where we are not yet able to address
       issue
    5. added pragma no cover for except block in tests as
       we ocasionally hit that in local test but won't happen in CI
    6. Removed a check for skipping mysql < 5.6 in a test since we
       don't test with it and it is EoL anyways.

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -63,7 +63,10 @@ class TestAssigningUserClass(TestCase):
         assert isinstance(attr.property.columns[0].type, sa.Unicode)
 
 
-@mark.skipif("os.environ.get('DB') in ['sqlite', 'oracle']")
+@mark.skipif(
+    "os.environ.get('DB') in ['sqlite', 'oracle']",
+    reason="sqlite doesn't have a concept of schema for oracle refer below mentioned fixme!",
+)
 class TestAssigningUserClassInOtherSchema(TestCase):
     user_cls = "User"
 
@@ -79,8 +82,42 @@ class TestAssigningUserClassInOtherSchema(TestCase):
         self.User = User
 
     def create_tables(self):
-        self.connection.execute("DROP SCHEMA IF EXISTS other")
-        self.connection.execute("CREATE SCHEMA other")
+        try:
+            self.connection.execute("DROP SCHEMA IF EXISTS other")
+            self.connection.execute("CREATE SCHEMA other")
+        except sa.exc.DatabaseError:  # pragma: no cover
+            try:
+                # Create a User for Oracle DataBase as it does not have concept of schema
+                # ref: https://stackoverflow.com/questions/10994414/missing-authorization-clause-while-creating-schema # noqa E501
+                self.connection.execute("CREATE USER other identified by other")
+                # need to give privilege to create table to this new user
+                # ref: https://stackoverflow.com/questions/27940522/no-privileges-on-tablespace-users
+                self.connection.execute("grant all privileges TO other")
+                # FIXME: (cx_Oracle.DatabaseError) ORA-01031: insufficient privileges
+                #        it seems system doesn't have privilege to conenct to other
+                #        now when transaction tries to refer other.user is says insufficient table
+                #        but same tests passes in test_table_builder?
+                #        skipping for now
+                # E       sqlalchemy.exc.DatabaseError: (cx_Oracle.DatabaseError) ORA-01031: insufficient privileges # noqa E501
+                # E       [SQL:
+                # E       CREATE TABLE transaction (
+                # E               issued_at DATE,
+                # E               id NUMBER(19) NOT NULL,
+                # E               remote_addr VARCHAR2(50 CHAR),
+                # E               user_id VARCHAR2(255 CHAR),
+                # E               PRIMARY KEY (id),
+                # E               FOREIGN KEY(user_id) REFERENCES other."user" (id)
+                # E       )
+                # E
+                # E       ]
+            except sa.exc.DatabaseError as dbe:
+                if (
+                    "ORA-01920: user name 'OTHER' conflicts with another user or role name"
+                    not in dbe.__str__()
+                ):
+                    # NOTE: prior to oracle 23c we don't have concept of if not exists
+                    #       so we just try to create if fails we continue
+                    raise
         TestCase.create_tables(self)
 
     def test_can_build_transaction_model(self):
