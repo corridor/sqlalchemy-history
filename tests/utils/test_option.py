bee06d48954aa7151e086065c800a64f15c63150
Indivar Mishra <indimishra@gmail.com>
2023-08-20 16:51:54 +0530

    chore: increase coverage for util file

---
+++ tests/utils/test_option.py
@@ -0,0 +1,26 @@
+import pytest
+import sqlalchemy as sa
+from sqlalchemy_history.utils import option
+from tests import TestCase
+
+
+class TestOption(TestCase):
+    @pytest.mark.parametrize(
+        "attribute",
+        [
+            "versioning",
+            "base_classes",
+            "table_name",
+            "exclude",
+            "include",
+            "create_models",
+            "create_tables",
+            "transaction_column_name",
+            "end_transaction_column_name",
+            "operation_type_column_name",
+            "strategy",
+            "use_module_name",
+        ],
+    )
+    def test_option(self, attribute):
+        assert option(self.Article, attribute) == option(sa.orm.aliased(self.Article), attribute)
