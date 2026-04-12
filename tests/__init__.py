1487275e506a1c6dfdaa797d8a7574a1f3b54c1b
Ashwin A Nayar <ashwin5059198@gmail.com>
2026-04-12 12:57:20 +0530

    test: Use oracledb driver with oracle tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -36,7 +36,7 @@ def get_dns_from_driver(driver):  # pragma: no cover
     elif driver == "mssql":
         return "mssql+pymssql://sa:MSsql2022@localhost:1433"
     elif driver == "oracle":
-        return "oracle+cx_oracle://SYSTEM:Oracle2022@localhost:1521"
+        return "oracle+oracledb://SYSTEM:Oracle2022@localhost:1521"
     else:
         raise Exception("Unknown driver given: %r" % driver)
 
